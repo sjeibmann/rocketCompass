9b4c39832d6634aeb69e443f1c2080bcc95f7653
Sam Jeibmann <sam.jeibmann@gmail.com>
2020-05-20 09:32:44 -0700

    moving styles to app.css.js

--- src/App.js
+++ src/App.js
@@ -1,51 +1,6 @@
 import React from 'react';
 import './App.css';
-import styled from 'styled-components';
-
-const Wrapper = styled.svg`
-  width:300px;
-  height:300px;
-  display: "flex";
-  justifyContent: "center";
-  alignItems: "center";
-  // box-shadow: 0 10px 40px 0 rgba(118,118,118,0.27);
-
-  #rocketCompass line{
-    visibility:hidden;
-    opacity:10;
-    stroke-dasharray:100;
-  }
-
-  line.#Jet2{
-    animation-delay:.5s;
-  }
-  
-  line.#Jet3{
-    animation-delay:.25s;
-  }
-  
-  :hover #rocketCompass line{
-    visibility: visible;
-    animation: animate .65s ease-in-out;
-  }
-  
-  @keyframes animate{
-      0%{
-        opacity:0;
-        stroke-dasharray:10;
-        stroke-dashoffset:10;
-      }
-
-      66%{
-        opacity:10;
-        // stroke-dashoffset:15;
-      }
-
-      100%{
-        opacity:100;
-      }
-    }
-`;
+import Wrapper from './App.css.js';
 
 function App() {
   return (
